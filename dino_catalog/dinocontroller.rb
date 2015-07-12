ac150171196c6b895873ec6cab1abdb1ef667a27
Raj Singh <raj@foreversingh.com>
2015-07-12 11:18:16 -0500

    style changes.

--- dino_catalog/dinocontroller.rb
+++ dino_catalog/dinocontroller.rb
@@ -1,17 +1,20 @@
 def controller_home(input)
   case input
-  when "view"
-    view_show_dinos(@dinodex.dinosaurs)
-  when "save"
-    view_save_files
-  when "add"
-    view_add_dino
-  when "search"
-    view_search_dinos
-  when "exit"
-    exit
+    when "view"
+      view_show_dinos(@dinodex.dinosaurs)
+    when "save"
+      view_save_files
+    when "add"
+      view_add_dino
+    when "search"
+      view_search_dinos
+    when "exit"
+      exit
   else
-    puts "That's not a valid option!"
+    begin
+      raise "That's not a valid option!"
+    rescue
+    end
     view_home
   end
 end
@@ -19,43 +22,49 @@ end
 def controller_add_dino(dino, response)
   if response == "y" || response == "yes"
     @dinodex.add(dino)
-    puts "Dinosaur Added!"
+    @messages = "Dinosaur Added!"
     view_home
   else
-    puts "Dinosaur has not been added..."
+    @messages = "Dinosaur has not been added..."
     view_home
   end
 end
 
 def controller_save_file(location)
   if @dinodex.dinosaurs.empty?
-    puts "You have no data to save!"
+    begin
+      raise "You have no data to save!"
+    rescue
+    end
     view_home
   else
     @dinodex.save(location)
-    puts "Save Successful"
+    @messages = "Save Successful"
     view_home
   end
 end
 
 def controller_search_dinos(input)
   case input
-  when "carnivores"
-    view_show_dinos(@dinodex.find_carnivores)
-  when "bipedal"
-    view_show_dinos(@dinodex.find_bipeds)
-  when "periods"
-    available_periods = []
-    @dinodex.dinosaurs.each{ |dino| available_periods << dino.period }
-    avaialble_periods = available_periods.uniq!
-    view_period_picker(available_periods)
-  when "size"
-    sizes = ['big', 'small']
-    view_size_picker(sizes)
-  when "home"
-    view_home
+    when "carnivores"
+      view_show_dinos(@dinodex.find_carnivores)
+    when "bipedal"
+      view_show_dinos(@dinodex.find_bipeds)
+    when "periods"
+      available_periods = []
+      @dinodex.dinosaurs.each{ |dino| available_periods << dino.period }
+      avaialble_periods = available_periods.uniq!
+      view_period_picker(available_periods)
+    when "size"
+      sizes = ["small", "big"]
+      view_size_picker(sizes)
+    when "home"
+      view_home
   else
-    puts "That's not a valid option!"
+    begin
+      raise "That's not a valid option!"
+    rescue
+    end
     view_search_dinos
   end
 end
