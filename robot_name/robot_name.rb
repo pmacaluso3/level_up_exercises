ee03ef41cd12e9d1b9fe421f93d1abb259b3fff6
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-29 14:49:22 -0500

    refactor for small, clear methods

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -7,19 +7,54 @@ class Robot
 
   def initialize(args = {})
     @@registry ||= []
-    @name_generator = args[:name_generator]
+    @name_generator = args[:name_generator] || NameGenerator.new
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
+    generate_unique_name
 
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    # I think this error is unneccessary, but it was included in the original code
+    if name_invalid?
+      raise NameCollisionError, 'There was a problem generating the robot name!'
     end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+    add_name_to_registry
+  end
+
+  def generate_unique_name
+    loop do 
+      # raise NameCollisionError
+      # the instructions said to make it more robust in case of a collision error
+      @name = @name_generator.generate_name
+      break if name_available?
+    end
+  end
+
+  def add_name_to_registry
     @@registry << @name
+  end
+
+  def name_available?
+    !(@@registry.include?(name))
+  end
+
+  def name_invalid?
+    !(name =~ /[a-zA-Z]{2}[\d]{3}/)
+  end
+end
+
+class NameGenerator
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_num
+    rand(10).to_s
+  end
+
+  def generate_name
+    output_name = ""
+    2.times { output_name += generate_char }
+    3.times { output_name += generate_num }
+    output_name
   end
 end
 
