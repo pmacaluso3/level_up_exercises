bcaa2a08fb72799550526a3c9b4d6aae7a8fbec6
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-30 11:06:40 -0500

    refactored robot namer based on bfeng's suggestions

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,67 +1,84 @@
 class NameCollisionError < RuntimeError; end
+class InvalidNameError < RuntimeError; end
 
 class Robot
   attr_accessor :name
+  attr_reader :default_name_generator, :name_generator
 
   @@registry
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator] || NameGenerator.new
+    @@registry ||= ["AB123"]
+    @name_generator = args[:name_generator] || DefaultNameGenerator.new
+    @default_name_generator = DefaultNameGenerator.new
 
     generate_unique_name
 
-    # I think this error is unneccessary, but it was included in the original code
-    if name_invalid?
-      raise NameCollisionError, 'There was a problem generating the robot name!'
+    unless name_valid?
+      raise InvalidNameError, 'There was a problem generating the robot name!'
     end
 
     add_name_to_registry
   end
 
-  def generate_unique_name
+  def generate_unique_default_name
     loop do 
-      # raise NameCollisionError
-      # the instructions said to make it more robust in case of a collision error
-      @name = @name_generator.generate_name
+      self.name = default_name_generator.call
       break if name_available?
     end
   end
 
+  def generate_unique_name
+    begin
+      self.name = name_generator.call
+      unless name_available?
+        p "Attempted to use name #{name}, but it was taken"
+        raise NameCollisionError, "That name is already taken"
+      end
+    rescue NameCollisionError
+      generate_unique_default_name
+    end
+  end
+
   def add_name_to_registry
-    @@registry << @name
+    @@registry << name
   end
 
   def name_available?
-    !(@@registry.include?(name))
+    !@@registry.include?(name)
   end
 
-  def name_invalid?
-    !(name =~ /[a-zA-Z]{2}[\d]{3}/)
+  def name_valid?
+    name =~ /[a-zA-Z]{2}[\d]{3}/
   end
 end
 
-class NameGenerator
-  def generate_char
-    ('A'..'Z').to_a.sample
+class DefaultNameGenerator
+  def generate_char_lambda
+    -> { ('A'..'Z').to_a.sample }
   end
 
-  def generate_num
-    rand(10).to_s
+  def generate_num_lambda
+    -> {rand(10).to_s }
   end
 
-  def generate_name
+  def call
     output_name = ""
-    2.times { output_name += generate_char }
-    3.times { output_name += generate_num }
+    2.times { output_name += generate_char_lambda.call }
+    3.times { output_name += generate_num_lambda.call }
     output_name
   end
 end
 
+
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
-# Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+# Handled with generat_unique_name
+generator = -> { 'AA111' }
+Robot.new(name_generator: generator)
+Robot.new(name_generator: generator)
+
+# Should raise InvalidNameError
+bad_generator = -> { "AB" }
+Robot.new(name_generator: bad_generator)
