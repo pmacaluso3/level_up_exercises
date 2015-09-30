a7ee5304b3682fbd949553c5b0121a1fc1c43ef4
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-30 10:14:08 -0500

    refactored arrowhead based on bfeng's comments

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -15,10 +15,13 @@ class Arrowhead
   }
 
   def self.classify(region, test_shape)
-    possible_shapes = CLASSIFICATIONS[region]
-    raise "Unknown region, please provide a valid region." if possible_shapes == nil
-    if this_shape = possible_shapes.include?(test_shape)
+    unless (possible_shapes = CLASSIFICATIONS[region])
+      raise "Unknown region, please provide a valid region."
+    end
+    if (this_shape = possible_shapes[test_shape])
       "You have a(n) '#{this_shape}' arrowhead. Probably priceless."
+    else
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
     end
   end
 end
