2164859da26383750b841f898506f56aa549b4dc
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-30 11:07:00 -0500

    rubocop refactor on robot name

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -5,39 +5,33 @@ class Robot
   attr_accessor :name
   attr_reader :default_name_generator, :name_generator
 
-  @@registry
-
   def initialize(args = {})
-    @@registry ||= ["AB123"]
+    @@registry ||= []
     @name_generator = args[:name_generator] || DefaultNameGenerator.new
     @default_name_generator = DefaultNameGenerator.new
 
     generate_unique_name
 
-    unless name_valid?
-      raise InvalidNameError, 'There was a problem generating the robot name!'
-    end
+    raise InvalidNameError, 'There was a problem generating the robot name!' unless name_valid?
 
     add_name_to_registry
   end
 
   def generate_unique_default_name
-    loop do 
+    loop do
       self.name = default_name_generator.call
       break if name_available?
     end
   end
 
   def generate_unique_name
-    begin
-      self.name = name_generator.call
-      unless name_available?
-        p "Attempted to use name #{name}, but it was taken"
-        raise NameCollisionError, "That name is already taken"
-      end
-    rescue NameCollisionError
-      generate_unique_default_name
+    self.name = name_generator.call
+    unless name_available?
+      p "Attempted to use name #{name}, but it was taken"
+      raise NameCollisionError, "That name is already taken"
     end
+  rescue NameCollisionError
+    generate_unique_default_name
   end
 
   def add_name_to_registry
@@ -59,7 +53,7 @@ class DefaultNameGenerator
   end
 
   def generate_num_lambda
-    -> {rand(10).to_s }
+    -> { rand(10).to_s }
   end
 
   def call
@@ -70,7 +64,6 @@ class DefaultNameGenerator
   end
 end
 
-
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
@@ -80,5 +73,5 @@ Robot.new(name_generator: generator)
 Robot.new(name_generator: generator)
 
 # Should raise InvalidNameError
-bad_generator = -> { "AB" }
-Robot.new(name_generator: bad_generator)
+# bad_generator = -> { "AB" }
+# Robot.new(name_generator: bad_generator)
