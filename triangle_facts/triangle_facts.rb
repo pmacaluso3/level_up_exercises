18daf1c1e06b32fdf63eba9b475b64e5468ab620
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-30 10:06:41 -0500

    convert tabs to spaces

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,78 +1,78 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-	attr_reader :side1, :side2, :side3
+  attr_reader :side1, :side2, :side3
 
-	def initialize(side1, side2, side3)
-		@side1, @side2, @side3 = side1, side2, side3
-	end
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+  end
 
-	def recite_facts
-		puts side_length_sentence
-		puts angle_sentences
-	end
+  def recite_facts
+    puts side_length_sentence
+    puts angle_sentences
+  end
 
-	def calculate_angles(a, b, c)
-		angleA = calculate_single_angle(b, c, a)
-		angleB = calculate_single_angle(c, a, b)
-		angleC = calculate_single_angle(a, b, c)
+  def calculate_angles(a, b, c)
+    angleA = calculate_single_angle(b, c, a)
+    angleB = calculate_single_angle(c, a, b)
+    angleC = calculate_single_angle(a, b, c)
 
-		[angleA, angleB, angleC]
-	end
-	
-	private
-	def equilateral
-		side1 == side2 && side2 == side3
-	end
+    [angleA, angleB, angleC]
+  end
+  
+  private
+  def equilateral
+    side1 == side2 && side2 == side3
+  end
 
-	def isosceles
-		[side1,side2,side3].uniq.length == 2
-	end
+  def isosceles
+    [side1,side2,side3].uniq.length == 2
+  end
 
-	def scalene
-		(equilateral || isosceles) ? false : true
-	end
+  def scalene
+    (equilateral || isosceles) ? false : true
+  end
 
-	def side_length_sentence
-		if equilateral
-			'This triangle is equilateral!'
-		elsif isosceles
-			'This triangle is isosceles! Also, that word is hard to type.'
-		elsif scalene
-			'This triangle is scalene and mathematically boring.'
-		end
-	end
+  def side_length_sentence
+    if equilateral
+      'This triangle is equilateral!'
+    elsif isosceles
+      'This triangle is isosceles! Also, that word is hard to type.'
+    elsif scalene
+      'This triangle is scalene and mathematically boring.'
+    end
+  end
 
-	def angle_sentences
-		angles = calculate_angles(side1,side2,side3)
+  def angle_sentences
+    angles = calculate_angles(side1,side2,side3)
 
-		if angles.include? 90
-			right = "This triangle is also a right triangle!\n" 
-		else
-			right = "\n"
-		end
+    if angles.include? 90
+      right = "This triangle is also a right triangle!\n" 
+    else
+      right = "\n"
+    end
 
-		"The angles of this triangle are #{angles.join(',')}\n#{right}"
-	end
+    "The angles of this triangle are #{angles.join(',')}\n#{right}"
+  end
 
-	# solving for C in a**2 + b**2 - 2abcosC = c**2
-	def calculate_single_angle(leg_1, leg_2, opposite_from_angle_of_interest)
-		numerator = (leg_1**2 + leg_2**2 - opposite_from_angle_of_interest**2)
-		denominator = 2.0*leg_1*leg_2
-		angle_in_rads = Math.acos(numerator / denominator)
-		radians_to_degrees(angle_in_rads)
-	end
+  # solving for C in a**2 + b**2 - 2abcosC = c**2
+  def calculate_single_angle(leg_1, leg_2, opposite_from_angle_of_interest)
+    numerator = (leg_1**2 + leg_2**2 - opposite_from_angle_of_interest**2)
+    denominator = 2.0*leg_1*leg_2
+    angle_in_rads = Math.acos(numerator / denominator)
+    radians_to_degrees(angle_in_rads)
+  end
 
-	def radians_to_degrees(rads)
-		return (rads * 180 / Math::PI).round
-	end
+  def radians_to_degrees(rads)
+    return (rads * 180 / Math::PI).round
+  end
 end
 
 
 triangles = [
-	[5,5,5],
-	[5,12,13],
+  [5,5,5],
+  [5,12,13],
 ]
 triangles.each { |sides|
-	tri = Triangle.new(*sides)
-	tri.recite_facts
+  tri = Triangle.new(*sides)
+  tri.recite_facts
 }
