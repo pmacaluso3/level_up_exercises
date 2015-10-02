b52ebc48d926bb30625329ce4de99c4beff8b827
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-10-02 10:30:00 -0500

    soften tabs

--- overlord/overlord.rb
+++ overlord/overlord.rb
@@ -6,31 +6,31 @@ require_relative 'models/bomb'
 enable :sessions
 
 get '/' do
-	redirect :"/bomb/new"
+  redirect :"/bomb/new"
 end
 
 get '/bomb/new' do
-	erb :new
+  erb :new
 end
 
 post '/code' do
-	BOMB.enter_code(params[:code])
-	redirect :"/bomb/show"
+  BOMB.enter_code(params[:code])
+  redirect :"/bomb/show"
 end
 
 post '/bomb' do
-	codes = params[:codes]
-	act = codes[:activation]
-	act = "1234" if act == ""
-	deact = codes[:deactivation]
-	deact = "0000" if deact == ""
-	BOMB = Bomb.new(act, deact)
-	redirect :"/bomb/show"
+  codes = params[:codes]
+  act = codes[:activation]
+  act = "1234" if act == ""
+  deact = codes[:deactivation]
+  deact = "0000" if deact == ""
+  BOMB = Bomb.new(act, deact)
+  redirect :"/bomb/show"
 end
 
-get	'/bomb/show' do
-	@bomb = BOMB
-	erb :"show"
+get '/bomb/show' do
+  @bomb = BOMB
+  erb :"show"
 end
 
 # we can shove stuff into the session cookie YAY!
