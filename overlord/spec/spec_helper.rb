9493a27076e788e05916619c4d7c5238d35bf1e6
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-10-02 10:18:45 -0500

    did the bomb assignment, but not tdd

--- overlord/spec/spec_helper.rb
+++ overlord/spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require "simplecov"
 SimpleCov.start
 
-require_relative "../lib/bomb.rb"
-require_relative "../lib/timer.rb"
-require_relative "../lib/wire.rb"
-require_relative "../lib/wire_bundle.rb"
+require_relative "../models/bomb.rb"
+# require_relative "../models/timer.rb"
+# require_relative "../models/wire.rb"
+# require_relative "../models/wire_bundle.rb"
