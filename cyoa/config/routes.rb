3cc48e9592e0dc6bfbd9aaf2d12ed7c957457a2f
Lesley <lkompare87@gmail.com>
2015-10-09 10:34:07 -0500

    Round Quotes are showing on Rounds Show page

--- cyoa/config/routes.rb
+++ cyoa/config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :create]
   post '/login' => 'users#login'
-
-  resources :games, only: [:show]
+  post '/games' => 'games#create'
+  resources :rounds, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
